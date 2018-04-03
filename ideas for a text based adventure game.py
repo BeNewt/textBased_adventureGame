d133cb7c885eb8d0a8714ed986463ebb84ff2157
Beth Newton <32069726+BeNewt@users.noreply.github.com>
2018-04-03 11:13:17 -0400

    Ideas for a text based adventure game

---
+++ ideas for a text based adventure game.py
@@ -0,0 +1,13 @@
+# Class for wrestlers
+# Class for match stipulations
+# Class for weapons
+# Class for championships
+# Class for match types
+# win matches to gain championships
+# winning meatches increases health
+# losing matches decreases health
+# weapons are used to decrease health
+# match stipulations tell what condition the match has to be held in
+# examples; "I Quit" match, falls count anywhere match, iron man match
+# match types tell what kind of match will be held
+# examples; singles, tag, intergender, handicap 
